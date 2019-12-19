7329da11d32d78acc42cd35377aea134196d7f5d
Just Basumatary <32031294+just012@users.noreply.github.com>
2019-12-19 17:33:56 +0530

    Add files via upload

--- MavenTesting/src/test/java/TestCases/TestFindingElements.java
+++ MavenTesting/src/test/java/TestCases/TestFindingElements.java
@@ -1,17 +1,33 @@
 package TestCases;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestFindingElements {
 	public static WebDriver driver;
 	public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		driver.get("http://gmail.com");
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//WebDriverWait wait= new WebDriverWait(driver, 5);
+		
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+			       .withTimeout(Duration.ofSeconds(10))
+			       .pollingEvery(Duration.ofSeconds(2))
+			       .withMessage("Time Out after 30 seconds.")
+			       .ignoring(NoSuchElementException.class);
 		
 //		WebElement username=driver.findElement(By.id("identifierId"));
 //		username.sendKeys("basumataryjust@gmail.com");
@@ -19,8 +35,16 @@ public class TestFindingElements {
 //		WebElement nextbtn=driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span"));
 //		nextbtn.click();
 		
-		driver.findElement(By.id("identifierId")).sendKeys("basumataryjust@gmail.com");
+		//driver.findElement(By.id("identifierId")).sendKeys("basumataryjust@gmail.com");
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("identifierId"))).sendKeys("basumataryjust@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
+		
+		//driver.findElement(By.name("password")).sendKeys("abcdefg");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"))).sendKeys("abcdefg");
+		driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/span/span")).click();
+		
+		//System.out.println(driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[2]/div[2]/span")).getText());
+		System.out.println(driver.getTitle());
 	}
 
 }
